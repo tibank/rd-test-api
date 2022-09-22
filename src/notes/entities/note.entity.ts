7ce55d0a0fbd003d5dbb4ad4c7944a7eca6d126e
Volodymyr Lysenko <tibank2071@gmail.com>
2022-09-22 09:09:13 +0300

    feat: add validator for note entity

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,11 +1,20 @@
 import { Category } from './category.entyty';
+import { Allow, IsBoolean, IsDate, IsInt, IsNotEmpty, Min, ValidateNested } from 'class-validator';
 
 export class Note {
+  @IsInt()
+  @Min(1)
   id: number;
+  @IsNotEmpty()
   name: string;
+  @IsDate()
   created: Date = new Date();
+  @ValidateNested()
   category: Category;
+  @Allow()
   content: string;
+  @IsBoolean()
+  @Allow()
   archived: boolean = false;
 
   constructor(partial: Partial<Note>) {
